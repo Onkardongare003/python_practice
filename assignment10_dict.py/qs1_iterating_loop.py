5e6393c75cc9a31e554abaf236f3d91dfe570f66
Onkar <onkardongare03@gmail.com>
2026-01-07 10:29:42 +0530

    iterating the loop

---
+++ assignment10_dict.py/qs1_iterating_loop.py
@@ -0,0 +1,11 @@
+# Iterating loop in dictionary
+
+d1={"name":"shruti","age":25,"address":"nashik","pin":42209}
+for i,j in d1.items():
+   print(i,j)
+for i in d1:
+   print(i)
+for i in d1.keys():
+   print(i)
+for i in d1.values():
+   print(i)
