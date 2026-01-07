52cdecb3e5f2df9f84a71b1dddf8b20a9b8916a3
Onkar <onkardongare03@gmail.com>
2026-01-07 08:28:32 +0530

    dictionart are created

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+# large amounnt of data srored in one single variable
+# elemet are stoed key and value
+# syntax
+# a={"key":"value"}
+
+d1={"name":"shruti","age":25,"address":"nashik","pin":42209}
+print(d1)
+print(d1['name'])
+print(d1['pin'])
+print(d1['age'])
