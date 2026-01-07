b7673d685b575c8fc5b5c24e33e1a4daa8e41a4d
Onkar <onkardongare03@gmail.com>
2026-01-07 10:51:27 +0530

    making the studnet list

--- assignment10_dict.py/qs1_iterating_loop.py
+++ assignment10_dict.py/qs1_iterating_loop.py
@@ -1,11 +1,11 @@
 # Iterating loop in dictionary
 
-d1={"name":"shruti","age":25,"address":"nashik","pin":42209}
+d1={"name":"shruti","age":"23","address":"nashik","pincode":422009}
 for i,j in d1.items():
-   print(i,j)
+   print(i,j,end="")
 for i in d1:
    print(i)
-for i in d1.keys():
-   print(i)
-for i in d1.values():
-   print(i)
+# for i in d1.keys():
+#    print(i,end="")
+# for i in d1.values():
+#    print(i,end="")
---
+++ assignment10_dict.py/qs2_practice.py
@@ -0,0 +1,12 @@
+# making the student name address age and its percentage
+
+d={
+    "name":["onkar","kiran","prasad","ganesh"],
+    "address":["nsk","a.nager","abc","xyz"],
+    "age":[22,18,19,20],
+    "percetage":[99,0,45.5,50,67]
+}
+print(d['name'])
+print(d['address'])
+print(d['age'])
+print(d['percetage'])
