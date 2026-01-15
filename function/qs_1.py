a955a55ec6abcac063d9d22bdd5b62994a70a7cd
Onkar <onkardongare03@gmail.com>
2026-01-15 12:19:02 +0530

    user define function are created

--- function/qs_1.py
+++ function/qs_1.py
@@ -9,3 +9,12 @@
 # Predefined functions→ functions already defined by the developers
 # E.g input(),print(),append(),len()
 # User defined functions →when programmers prepare functions for their own sake of use and call whenever required is call UDF
+
+# def add():
+#     a=int(input("Enter the Number = "))
+#     b=int(input("enter the 2nd Number = "))
+#     c=a+b
+#     print("the addtion is = ",c)
+# add()
+
+
