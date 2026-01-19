2bb65cfcd403aa22510fefd959eeb317ac4275c3
Onkar <onkardongare03@gmail.com>
2026-01-19 14:10:23 +0530

    uadating the column in csv file

--- csvpractice.py
+++ csvpractice.py
@@ -1,6 +1,6 @@
 import pandas
 df=pandas.read_csv('people100.csv')
-# print(df)
+print(df)
 # print(df.head)#printing the 1st 5 row
 # print(df.tail)#printing the last 5 row
 
@@ -11,4 +11,21 @@ df=pandas.read_csv('people100.csv')
 # print(df[df['Sex']=='Male'])
 # print=df[df['Job Title']=="Economist"]
 
-print(df['User Id'])
+# # print(df['User Id'])
+
+# d=df[df['Job Title']=="Economist"]
+# print(df[df["Age"]>21])
+# print(df(df['age']>21) & (df['city']=="pune"))
+
+
+# df.sort_values("age")
+
+
+#updating the exting the column
+
+df['index']=df['Index']+55
+print(df)
+
+
+
+
