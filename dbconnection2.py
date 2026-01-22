6584ec1ec02cfe7f22cc58929962aa3825e433ca
Onkar <onkardongare03@gmail.com>
2026-01-22 22:03:08 +0530

    sloving the error and add connect to the data to the workbench and add data in dbms

--- dbconnection2.py
+++ dbconnection2.py
@@ -14,25 +14,25 @@
 # Step 6:
 # Commit the changes
 # databaseobjectname.commit()
+import mysql.connector
 
-
-import mysql.connector  
-db=mysql.connector.connect(host="localhost",user="root",password="root",database="employee")
-
+db=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="onkar",
+    database="employee"
+)
 
 mycursor=db.cursor()
 
+id=int(input("enter the id of the employee = "))
+name=input("enter the name of the employee = ")
+salary=int(input("enter the salary of the employee = "))
+age=int(input("enter the age of the employee = "))
+address=input("enter the address of the employee = ")
 
-id=int(input("enter the id of the employee"))
-name=input("enter the name of the employee")
-salary=int(input("enter the salary of the employee"))
-age=int(input("enter the age of the employee"))
-address=input("enter the address of the employee")
 s="insert into emp(empid,empname,empsalary,empage,empaddress)values(%s,%s,%s,%s,%s)"
 v=(id,name,salary,age,address)
 
-
 mycursor.execute(s,v)
-
-
 db.commit()
