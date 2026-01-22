02f27e78163c5adcb22f108f49e9ce54033414e0
Onkar <onkardongare03@gmail.com>
2026-01-22 09:09:13 +0530

    learning to coneect workbench to the python file

--- dbconnection.py
+++ dbconnection.py
@@ -1,6 +1,16 @@
-import mysql.connector
-
-
-db = mysql.connector.connect(host="localhost",  user="root",password="onkar",database="employee",port=3307
-)
-print(db)
+# Step 1:
+# Import the module
+# Import mysql.connector
+# Step2: 
+# databaseobject=mysql.connector.connect(host=”localhost”,user=”root”,password=”root”,database=”databasename”_
+# Step3 : 
+# Create the cursor
+# cursorobject=databaseobject.cursor()
+# Step4 : 
+# Write the query and also provide the values which are going to use in query
+# Step 5: 
+# Call the execute function
+# cursorobject.execute(query,values)
+# Step 6:
+# Commit the changes
+# databaseobjectname.commit()
