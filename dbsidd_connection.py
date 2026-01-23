284ae2cf80457f7e679d790dc2172aa3ee750f4d
Onkar <onkardongare03@gmail.com>
2026-01-23 16:28:13 +0530

    Solving the mistak

--- dbsidd_connection.py
+++ dbsidd_connection.py
@@ -1,8 +1,8 @@
 import mysql.connector
 db=mysql.connector.connect(
-    host="localhost"
-    user="root"
-    password="onkar"
+    host="localhost",
+    user="root",
+    password="onkar",
     database="siddhika"
 )
 
@@ -11,7 +11,7 @@ id=int(input("Enter the your id = "))
 salary=int(input("Enter Your Salary = "))
 age=int(input("Enter the Your age = "))
 address=input("Enter your address = ")
-eduacation=int(input("Enter your address = "))
+eduacation=int(input("Enter your Eduaction = "))
 
 
 s="insert into sidd(empid,empsalary,empage,empaddress,empeduaction) values(%s,%s,%s,%s,%s)"
